e4c8e87101f13452005ab358e3bac9a72024b283
piyada <waengsothon_p@silpakorn.edu>
2020-01-13 08:11:32 +0700

    final

--- app/src/main/java/th/ac/su/ict/discountcalculator/MainActivity.kt
+++ app/src/main/java/th/ac/su/ict/discountcalculator/MainActivity.kt
@@ -2,11 +2,34 @@ package th.ac.su.ict.discountcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val originalpc = findViewById<EditText>(R.id.originalpc)
+        val percent = findViewById<EditText>(R.id.percent)
+        val btcal = findViewById<Button>(R.id.btcal)
+        val shprice = findViewById<TextView>(R.id.shprice)
+        val checkbox = findViewById<CheckBox>(R.id.checkbox)
+
+        btcal.setOnClickListener() {
+            var originalprice:Float = originalpc.text.toString().toFloat()
+            var percentoff:Float = percent.text.toString().toFloat()
+            var resultcal = (originalprice * percentoff) / 100
+            var resultprice: Float = 0F
+                resultprice = originalprice - percentoff
+
+            if (checkbox.isChecked) {
+                resultprice += (resultprice / 100)*7
+            }
+            shprice.setText(resultprice.toString())
+        }
     }
 }
